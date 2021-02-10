8b1f0cb9b018dbc145958503c13e0adea1e2f8fd
ZobovaA <75171940+ZobovaA@users.noreply.github.com>
2021-02-10 18:48:00 +0200

    скрипт для модалки

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,25 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    
+    openMobileMenuBtn: document.querySelector('[data-mobile-menu-open]'),
+    closeMobileMenuBtn: document.querySelector('[data-mobile-menu-close]'),
+    mobileMenu: document.querySelector('[data-mobile-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+  refs.openMobileMenuBtn.addEventListener('click', toggleMobileMenu);
+  refs.closeMobileMenuBtn.addEventListener('click', toggleMobileMenu);
+
+  function toggleMobileMenu() {
+    refs.mobileMenu.classList.toggle('is-hidden');
+  }
+})();
